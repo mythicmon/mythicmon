891dd8a1435447e2242628c6d3976555801d8f36
mythicmon <potatogorf@gmail.com>
2026-07-05 11:18:03 -0400

    Refactor Creature class to include state management

--- src/game/objects/Creature.ts
+++ src/game/objects/Creature.ts
@@ -1,24 +1,59 @@
 import Phaser from "phaser";
 
-export class Creature extends Phaser.Physics.Arcade.Sprite{
+export type CreatureState =
+    | "idle"
+    | "wander"
+    | "notice"
+    | "flee"
+    | "attack";
 
-    species:string;
+export class Creature extends Phaser.Physics.Arcade.Sprite {
+
+    species: string;
+    state: CreatureState = "idle";
+
+    private moveTimer = 0;
+    private readonly WALK_SPEED = 70;
 
     constructor(
-        scene:Phaser.Scene,
-        x:number,
-        y:number,
-        texture:string,
-        species:string
-    ){
+        scene: Phaser.Scene,
+        x: number,
+        y: number,
+        texture: string,
+        species: string
+    ) {
 
-        super(scene,x,y,texture);
+        super(scene, x, y, texture);
 
-        this.species=species;
+        this.species = species;
 
         scene.add.existing(this);
-
         scene.physics.add.existing(this);
+
+        this.setCollideWorldBounds(true);
+
+    }
+
+    update(time: number) {
+
+        if (time > this.moveTimer) {
+
+            this.moveTimer = time + Phaser.Math.Between(1500, 4000);
+
+            const angle = Phaser.Math.FloatBetween(
+                0,
+                Math.PI * 2
+            );
+
+            this.scene.physics.velocityFromRotation(
+                angle,
+                this.WALK_SPEED,
+                this.body.velocity
+            );
+
+            this.state = "wander";
+
+        }
 
     }
 
